346c2ccd2329805b1a8fbfaaf85242fb3f184e89
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2022-05-17 13:06:57 -0300

    Creating basic user interface

--- order_now/shop/views.py
+++ order_now/shop/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+products = [
+    {
+        'name': 'Coxinha de Frango',
+        'cost': 'R$13,00'
+    },
+    {
+        'name': 'Coxinha de Frango',
+        'cost': 'R$13,00'
+    },
+    {
+        'name': 'Coxinha de Frango',
+        'cost': 'R$13,00'
+    },
+    {
+        'name': 'Coxinha de Frango',
+        'cost': 'R$13,00'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Shop Home</h1>')
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'shop/home.html', context)
